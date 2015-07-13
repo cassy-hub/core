cec8bdd1a08f3668120cf4037370477fb592ed8a
Mark Wylde <markwylde@users.noreply.github.com>
2015-07-13 14:40:54 +0100

    Changes

--- src/build.sh
+++ src/build.sh
@@ -1,6 +1,6 @@
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
-docker rm $(docker stop $(docker ps -a -f "IMAGE=cassy/core" -q))
+docker rm $(docker stop $(docker ps -a -f "IMAGE=cassyhub/core" -q))
 
-docker build -t cassy/core .
-docker run -it -p 8080:8080 -v $DIR:/src -d cassy/core
+docker build -t cassyhub/core .
+docker run -it -p 8080:8080 -v $DIR:/src -d cassyhub/core
