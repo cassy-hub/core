267398e46aa523d4e14ba0d590ee955f0d0b63ae
Stephen Scott <scottie1984@gmail.com>
2015-07-13 17:15:04 +0100

    Mongo

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
-docker rm $(docker stop $(docker ps -a -f "IMAGE=cassyhub/core" -q))
+docker rm $(docker stop $(docker ps -a -f "name=cassyhub-core" -q))
 
 docker build -t cassyhub/core .
-docker run -it -p 8080:8080 -v $DIR/src:/app/src -d cassyhub/core
+docker run --name cassyhub-core -it -p 8080:8080 -v $DIR/src:/app/src -d cassyhub/core
