575e5eb6cd28d96a0b8f4db8b2b7bd441c000d60
Mark Wylde <markwylde@users.noreply.github.com>
2015-07-16 12:12:46 +0100

    More frontend changes

--- router/public/components/MemberHome.jsx
+++ router/public/components/MemberHome.jsx
@@ -4,55 +4,13 @@ define(function(require) {
 
   var React = require('react');
   var ReactBootstrap = require('react-bootstrap');
-  var ReactQuill = require('react-quill');
 
   var HeaderBar = require('jsx!components/globals/HeaderBar');
   var FooterBar = require('jsx!components/globals/FooterBar');
 
   var MemberHome = React.createClass({
-    getInitialState: function() {
-      return {
-        tag: '',
-        data: null
-      };
-    },
-
-    validationState() {
-      var length = this.state.tag.length;
-      if (length > 10) { return 'success'; }
-      else if (length > 5) { return 'warning'; }
-      else if (length > 0) { return 'error'; }
-    },
-
-    onTextChange: function(value) {
-      this.setState({ data: value });
-    },
-
-    onTagChange: function() {
-      this.setState({ tag: this.refs.input.getValue() });
-    },
-
-    handleClick: function() {
-      $.ajax({
-        url: '/api/documents',
-        method: 'post',
-        data: {
-          'tags': this.state.tag,
-          'content': this.state.data
-        },
-        dataType: 'json',
-        success: function(data) {
-          console.log('scooby', data);
-        },
-        error: function(xhr, status, err) {
-          console.log(status, err.toString());
-        }
-      });
-    },
 
     render: function() {
-      var Button = ReactBootstrap.Button;
-      var Input = ReactBootstrap.Input;
       var user = this.props.user;
 
       return (
--- router/public/components/documents/NewDocument.jsx
+++ router/public/components/documents/NewDocument.jsx
@@ -3,6 +3,7 @@ define(function(require) {
   var $ = require('jquery');
 
   var React = require('react');
+  var ReactRouter = require('react-router');
   var ReactBootstrap = require('react-bootstrap');
   var ReactQuill = require('react-quill');
 
@@ -10,39 +11,41 @@ define(function(require) {
   var FooterBar = require('jsx!components/globals/FooterBar');
 
   var NewDocument = React.createClass({
+    mixins: [ReactRouter.Navigation],
+
     getInitialState: function() {
       return {
+        title: '',
         tag: '',
-        data: ''
+        content: ''
       };
     },
 
-    validationState() {
-      var length = this.state.tag.length;
-      if (length > 10) { return 'success'; }
-      else if (length > 5) { return 'warning'; }
-      else if (length > 0) { return 'error'; }
-    },
-
-    onTextChange: function(value) {
-      this.setState({ data: value });
+    onTitleChange: function() {
+      this.setState({ title: this.refs.title.getValue() });
     },
 
     onTagChange: function() {
-      this.setState({ tag: this.refs.input.getValue() });
+      this.setState({ tag: this.refs.tag.getValue() });
+    },
+
+    onContentChange: function(value) {
+      this.setState({ content: value });
     },
 
     handleClick: function() {
+      var self = this;
       $.ajax({
         url: '/api/documents',
         method: 'post',
         data: {
+          'title': this.state.title,
           'tags': this.state.tag,
-          'content': this.state.data
+          'content': this.state.content
         },
         dataType: 'json',
-        success: function(data) {
-          console.log('scooby', data);
+        success: function() {
+          self.transitionTo('/');
         },
         error: function(xhr, status, err) {
           console.log(status, err.toString());
@@ -60,20 +63,28 @@ define(function(require) {
           <HeaderBar user={user}/>
           <div>
             <h3>Create a new document</h3>
-            <Input
-              type='text'
-              value={this.state.tag}
-              placeholder='Enter tag'
-              label='Enter a tag for your document'
-              bsStyle={this.validationState()}
-              hasFeedback
-              ref='input'
-              groupClassName='group-class'
-              labelClassName='label-class'
-              onChange={this.onTagChange} />
+              <Input
+                type='text'
+                value={this.state.title}
+                placeholder='Enter a title to briefly describe your document'
+                label='Document Title'
+                ref='title'
+                groupClassName='group-class'
+                labelClassName='label-class'
+                onChange={this.onTitleChange} />
+
+              <Input
+                type='text'
+                value={this.state.tag}
+                placeholder='Path'
+                label='Enter the path for storing your document'
+                ref='tag'
+                groupClassName='group-class'
+                labelClassName='label-class'
+                onChange={this.onTagChange} />
 
             <label>Enter your content:</label>
-            <ReactQuill theme='snow' value={this.state.data} onChange={this.onTextChange} />
+            <ReactQuill theme='snow' value={this.state.data} onChange={this.onContentChange} />
             <hr />
             <Button bsStyle='default' onClick={this.handleClick}>Cancel</Button>
             <Button bsStyle='success' className='pull-right' onClick={this.handleClick}>Insert Document</Button>
