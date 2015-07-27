0cfe6ee6b3edd1f83c81c8e230abded915cc9c22
Stephen Scott <s.p.scott@kainos.com>
2015-07-27 21:23:01 +0100

    Config of public js module

--- router/src/assets/js/cassy-hub.js
+++ router/src/assets/js/cassy-hub.js
@@ -1,17 +1,35 @@
 var CassyHub = (function () {
    var ch = {};
    var config = {};
+   var locale;
+
    ch.setup = function(options) {
      config.publickey = options.publickey;
      config.language = options.language || false;
+     config.defaultLanguage = options.defaultLanguage || "en";
+     config.supportedLanguages = options.supportedLanguages || ["en", "es"];
+     config.protocol = options.protocol || "http";
+     config.host = options.host || "cassyhub.io";
+     config.port = options.port || "80";
    }
+
+   ch.setLanguage = function(lang) {
+      locale = lang;
+   }
+
    ch.init = function () {
          $( ".cassy-hub" ).each(function() {
-              var language = (navigator.language || navigator.browserLanguage).split('-')[0];
+              if (locale === undefined) {
+                 locale = (navigator.language || navigator.browserLanguage).split('-')[0];
+                 if (config.supportedLanguages.indexOf(locale) === -1) {
+                    locale = config.defaultLanguage;
+                 }
+              };
+
               var self = this;
-              var reqUrl = "http://cassyhub.io:80/api-public/public-docs/" + this.dataset.tag;
+              var reqUrl = config.protocol + "://" + config.host + ":" + config.port + "/api-public/public-docs/" + this.dataset.tag;
               if (config.language) {
-                 reqUrl = reqUrl + "/" + language + "/"
+                 reqUrl = reqUrl + "/" + locale + "/"
               }
               $.ajax({
                   url: reqUrl,
